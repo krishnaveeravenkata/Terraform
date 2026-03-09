01cd8a0962d15306f1f3c649301c1e42e94b3d6a
krishnaveeravenkata <venkatakrishna12356@example.com>
2026-03-09 17:02:10 +0530

    Setup initial EC2 instance for Day-1

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -1,8 +1,8 @@
 # Day 1: Basic EC2 Instance Setup
 resource "aws_instance" "day1_vm" {
-  ami           = "ami-02dfbd4ff395f2a1b" # Standard Amazon Linux 2 AMI
+  ami           = "ami-02dfbd4ff395f2a1b" 
   instance_type = "t2.micro"
-
+  
   tags = {
     Name = "Day1-Instance"
   }
