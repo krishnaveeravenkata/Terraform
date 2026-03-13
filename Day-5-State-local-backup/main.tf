7c81c891ebb0719156e8483d91b557285b8ab360
krishnaveeravenkata <venkatakrishna12356@example.com>
2026-03-13 12:15:51 +0530

    Add Day-5 Terraform local backup

---
+++ Day-5-State-local-backup/main.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "kr_vpc"
+    }
+}
+
+
+resource "aws_instance" "name" {
+
+ami = "ami-02dfbd4ff395f2a1b"
+instance_type = "t3.micro"
+tags = {
+Name = "kr"
+}
+}
